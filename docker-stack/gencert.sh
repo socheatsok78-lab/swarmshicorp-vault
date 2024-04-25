b9501b896afa1a8a18104b4c6ff51a7033ed7a0a
Socheat Sok <alex@socheat.net>
2024-04-25 14:44:09 +0700

    Update deployment spec

--- docker-stack/gencert.sh
+++ docker-stack/gencert.sh
@@ -11,7 +11,7 @@ else
 fi
 
 if [ ! -f "out/$INTERNAL_VAULT_CERT.key" ]; then
-    certstrap request-cert --common-name $INTERNAL_VAULT_CERT --passphrase ""
+    certstrap request-cert --common-name $INTERNAL_VAULT_CERT --domain $INTERNAL_VAULT_CERT --passphrase ""
     if [ -f "out/$INTERNAL_VAULT_CERT.crt" ]; then
         echo "File \"out/$INTERNAL_VAULT_CERT.crt\" exists"
     else
@@ -20,7 +20,12 @@ if [ ! -f "out/$INTERNAL_VAULT_CERT.key" ]; then
 
     docker secret ls --filter name=vault_tls_key --format '{{.ID}}' | xargs docker secret rm
     docker secret create vault_tls_key out/$INTERNAL_VAULT_CERT.key
+fi
+
+if [ ! -f "out/${INTERNAL_VAULT_CERT}_fullchain.crt" ]; then
+    cat out/$INTERNAL_VAULT_CERT.crt > out/${INTERNAL_VAULT_CERT}_fullchain.crt
+    cat out/$INTERNAL_VAULT_CA_NAME.crt >> out/${INTERNAL_VAULT_CERT}_fullchain.crt
 
     docker secret ls --filter name=vault_tls_cert --format '{{.ID}}' | xargs docker secret rm
-    docker secret create vault_tls_cert out/$INTERNAL_VAULT_CERT.crt
+    docker secret create vault_tls_cert out/${INTERNAL_VAULT_CERT}_fullchain.crt
 fi
