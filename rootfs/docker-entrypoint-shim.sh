e1c9d3a18b17b8ca827df86fc0eec50c1c3da732
Socheat Sok <alex@socheat.net>
2024-04-25 11:35:04 +0700

    Generate self-signed cert for vault as default

--- rootfs/docker-entrypoint-shim.sh
+++ rootfs/docker-entrypoint-shim.sh
@@ -15,6 +15,27 @@ get_addr () {
       exit}'
 }
 
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		mysql_error "Both $var and $fileVar are set (but are exclusive)"
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
 if [ -n "$VAULT_API_INTERFACE" ]; then
     export VAULT_API_ADDR=$(get_addr $VAULT_API_INTERFACE ${VAULT_API_ADDR:-"https://0.0.0.0:8200"})
     export VAULT_ADDR=${VAULT_API_ADDR}
@@ -26,6 +47,10 @@ if [[ -z "${VAULT_RAFT_NODE_ID}" ]]; then
     export VAULT_RAFT_NODE_ID=$(hostname)
     echo "Using VAULT_RAFT_NODE_ID: $VAULT_RAFT_NODE_ID"
 fi
+
+# Initialize values that might be stored in a file
+file_env 'VAULT_TLS_KEY_FILE' '/certs/vault_tls_key_file.pem'
+file_env 'VAULT_TLS_CERT_FILE' '/certs/vault_tls_cert_file.pem'
 
 # run the original entrypoint
 exec docker-entrypoint.sh "${@}"
